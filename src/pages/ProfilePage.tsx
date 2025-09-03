226993890ce9e9baaa1b2b0eedd017b1e7e088df
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-03 20:57:02 +0200

    commit new changes

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function ProfilePage() {
+    return (
+        <div className="max-w-lg mx-auto bg-white p-6 rounded shadow">
+            <h2 className="text-xl font-semibold mb-4">Profile</h2>
+            <p className="text-sm text-slate-600">Profile view/edit will be implemented here</p>
+        </div>
+    )
+}

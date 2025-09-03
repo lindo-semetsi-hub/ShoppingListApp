4b21f5aa0a3a78f0866a16d9afdeca3246c47b26
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-03 20:59:58 +0200

    commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate, Link} from 'react-router-dom';
-import LoginPage from './pages/LoginPage.tsx';
+import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';

2df9289112e871801ee8ab8a61be0769874a5d1d
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-03 10:35:48 +0200

    commiting ncurrent changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react';
+import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client'
+import BrowserRouter from 'react-router-dom';
+import store from './store';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+      <App /></BrowserRouter>
+    </Provider>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);

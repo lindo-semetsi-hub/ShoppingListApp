3c210b0b5c6a860527c719a09d850555e9ba92e6
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-03 10:45:55 +0200

    commit new changes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,16 @@
-import { configureStore } from '@reduxjson'
+import { configureStore } from '@reduxjson/toolkit';
+import authReducer from './slices/authSlice';
+import listsReducer from  '.slices/listsSclice';
+
+const store = configureStore ({
+    reducer: {
+        auth: authReducer,
+        lists: listsReducer
+    }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
+

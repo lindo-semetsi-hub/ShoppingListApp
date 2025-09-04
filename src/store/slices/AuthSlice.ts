4b7623c301fbbbe1bf864523a49fc204fc5dfa07
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-04 12:45:12 +0200

    commit new changes

--- src/store/slices/AuthSlice.ts
+++ src/store/slices/AuthSlice.ts
@@ -3,50 +3,29 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 type User = {
     id: number;
     email: string;
+    password: string;
     name: string;
     surname: string;
     cell: string;
-    createdAt: string;
 };
-
-type AuthState = {
+interface AuthState {
     user: User | null;
-    token: string | null;
-    loading: boolean;
-    error?: string | null;
-};
-
+}
 const initialState: AuthState = {
     user: null,
-    token: null,
-    loading: false,
-    error: null
 };
 
 const authSlice = createSlice ({
     name: 'auth',
     initialState,
     reducers: {
-        startLoading(state) {
-            state.loading = true;
-            state.error = null;
-        },
-        loginSuccess(state, action: PayloadAction<{ user: User; token?: string }>) {
-            state.user = action.payload.user;
-            state.token = action.payload.token ?? null;
-            state.loading = false;
-            state.error = null;
+     login: (state, action: PayloadAction<Partial<User>>) => {
+            state.user = action.payload;
         },
         logout(state) {
             state.user = null;
-            state.token = null;
-            state.loading = false;
-            state.error = null;
         },
-        setError(state, action: PayloadAction<string>) {
-            state.error = action.payload;
-            state.loading = false;
-        },
+        
         updateProfile(state, action: PayloadAction<Partial<User>>) {
             if (state.user)
  {
@@ -56,5 +35,5 @@ const authSlice = createSlice ({
     }
 });
 
-export const {startLoading, loginSuccess, logout, setError, updateProfile} = authSlice.actions;
+export const {login, logout, updateProfile} = authSlice.actions;
 export default authSlice.reducer; 
